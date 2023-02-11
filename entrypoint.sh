a055f9dd2d3c53fe23b9c4c73d1423052d6cba35
grant0711 <adamsgrant87@gmail.com>
2023-02-11 11:51:45 +0000

    Add curl request to github api

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,7 @@ echo "service-name = $1"
 echo "service-directory = $2"
 echo "github_token = $3"
 
+curl --request GET \
+    --url "https://api.github.com/octocat" \
+    --header "Authorization: Bearer $3"
+
