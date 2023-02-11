087570e7080a4ec9116a99520bec2c7b200c46d2
grant0711 <adamsgrant87@gmail.com>
2023-02-11 11:55:26 +0000

    Add additional headers

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@ echo "github_token = $3"
 
 curl --request GET \
     --url "https://api.github.com/octocat" \
-    --header "Authorization: Bearer $3"
-
+    --header "Authorization: Bearer $3" \
+    --header "Accept: application/vnd.github+json" \
+    --header "X-GitHub-Api-Version: 2022-11-28"
