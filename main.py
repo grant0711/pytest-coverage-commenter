3157f12098a425c19abfa66aa97c63e229d19087
grant0711 <adamsgrant87@gmail.com>
2023-02-18 18:22:23 +0000

    Add skeleton of logic

--- main.py
+++ main.py
@@ -3,11 +3,55 @@ import sys
 
 import requests
 
+
+
+def format_coverage_report(r):
+    print(r)
+    return r
+
+def get_comment(service_name, github_repo, github_token, github_issue_number):
+    return ''
+
+def create_comment(service_name, github_repo, github_token, github_issue_number, coverage_report):
+    return
+
+def update_comment(service_name, github_repo, github_token, github_issue_number, coverage_report):
+    return
+
+def main():
+    service_name = sys.argv[1]
+    coverage_report = sys.argv[2]
+    github_token = sys.argv[3]
+    github_repo = sys.argv[4]
+    github_issue_number = sys.argv[5]
+
+    # Check to make sure we are on a pull request and not a push
+    print(github_issue_number)
+    print(type(github_issue_number))
+    if github_issue_number == 0:
+        return
+
+    # Format the coverage report
+    coverage_report = format_coverage_report(coverage_report)
+
+    # Find existing comment
+    comment_id = get_comment(service_name, github_repo, github_token, github_issue_number)
+
+    # If not found, create one
+    if not comment_id:
+        create_comment(service_name, github_repo, github_token, github_issue_number, coverage_report)
+
+    # If found, update the existing one
+    else:
+        update_comment(service_name, github_repo, github_token, github_issue_number, coverage_report)
+
+
+
+
 if __name__=="__main__":
-    print('hello')
-    print(sys.argv)
+    main()
 
-
+    
 # Where to find a comment:
 # GET
 # https://api.github.com/repos/{owner}/{repo}/issues/{pull number}/comments
