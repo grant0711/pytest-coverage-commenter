3fcdda16d0b375dc756cc519565a4a7b8c18a456
grant0711 <adamsgrant87@gmail.com>
2023-02-18 15:00:46 +0000

    Add app path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 #echo "coverage-report = $2"
 #echo "github_token = $3"
 
-python main.py $1 $2 $3
+python app/main.py $1 $2 $3
 
 #curl --request GET \
 #    --url "https://api.github.com/octocat" \
