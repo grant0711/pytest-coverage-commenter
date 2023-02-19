1c403e2e1c1b5ca36ba7a9c497313e2629ccac12
grant0711 <adamsgrant87@gmail.com>
2023-02-19 12:18:50 +0000

    Clean up main

--- main.py
+++ main.py
@@ -8,26 +8,15 @@ from github_api import (
 )
 
 def format_coverage_report(r):
-    print(r)
     return r
 
 def main(service_name, coverage_report, github_token, github_repo, github_issue_number):
-    # Check to make sure we are on a pull request and not a push
     if github_issue_number == '0':
         return
-
-    # Format the coverage report
     coverage_report = format_coverage_report(coverage_report)
-
-    # Find existing comment
     comment_id = get_comment(service_name, github_repo, github_token, github_issue_number)
-    print(comment_id)
-
-    # If not found, create one
     if not comment_id:
         create_comment(service_name, github_repo, github_token, github_issue_number, coverage_report)
-
-    # If found, update the existing one
     else:
         update_comment(service_name, github_repo, github_token, comment_id, coverage_report)
 
